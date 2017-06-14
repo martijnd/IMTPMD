90d0bfc75ea895101b7f61f8ae33dbc615dd5a57
martijnd <martijn.dorsman@gmail.com>
2017-06-14 13:16:23 +0200

    Mock up of the mainAcitivity and added a custom font widget

---
+++ app/src/main/java/nl/martijndorsman/imtpmd/TypefacedTextView.java
@@ -0,0 +1,33 @@
+package nl.martijndorsman.imtpmd;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Martijn on 14/06/17.
+ */
+
+public class TypefacedTextView extends android.support.v7.widget.AppCompatTextView {
+
+    public TypefacedTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        //Typeface.createFromAsset doesn't work in the layout editor. Skipping...
+        if (isInEditMode()) {
+            return;
+        }
+
+        TypedArray styledAttrs = context.obtainStyledAttributes(attrs, R.styleable.TypefacedTextView);
+        String fontName = styledAttrs.getString(R.styleable.TypefacedTextView_typeface);
+        styledAttrs.recycle();
+
+        if (fontName != null) {
+            Typeface typeface = Typeface.createFromAsset(context.getAssets(), fontName);
+            setTypeface(typeface);
+        }
+    }
+
+}
